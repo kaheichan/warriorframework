81d85ff103888b83fe31cc73d4bef568bd1e7534
kaheichan <kaheichan@utexas.edu>
2017-06-06 15:25:01 -0500

    update travis

--- wftests/pylint_travis/doc.sh
+++ wftests/pylint_travis/doc.sh
@@ -6,3 +6,10 @@ rm -rf docs/build/*
 #Rebuild package .rst files to make sure it reflect
 sphinx-apidoc -f -o  docs/source  warrior/Actions
 sphinx-apidoc -f -o  docs/source  warrior/Framework
+
+git config 
+
+git add docs
+git commit -m "update warriorframework rst documents"
+git config --list
+git push -u origin release
