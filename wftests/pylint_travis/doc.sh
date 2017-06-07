9e7b78816e75b6db99e41e7397a7752085f83758
kaheichan <kaheichan@utexas.edu>
2017-06-07 13:57:26 -0500

    testing encryped token

--- wftests/pylint_travis/doc.sh
+++ wftests/pylint_travis/doc.sh
@@ -9,8 +9,9 @@ sphinx-apidoc -f -o  docs/source  warrior/Framework
 
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis CI"
-git remote add origin-docs https://$GITHUB_TOKEN@github.com/kaheichan/warriorframework.git
+git remote add origin-docs https://${GITHUB_TOKEN}@github.com/kaheichan/warriorframework.git
 
+set +x
 git add docs
 git commit -m "update warriorframework rst documents"
 git config --list
